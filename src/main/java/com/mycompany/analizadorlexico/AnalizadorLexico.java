0f6cb5ffc9f77ba882deb6cec9ff2f2a0ded4691
Martin López <martinlopez45630@gmail.com>
2025-09-30 19:22:16 -0300

    lexico.java no anda

---
+++ src/main/java/com/mycompany/analizadorlexico/AnalizadorLexico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.analizadorlexico;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AnalizadorLexico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
